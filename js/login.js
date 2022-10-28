4ea7fda2245f7f15272e5725d5db65b06da6009d
Juanpi92 <juan.urgelles92@gmail.com>
2022-10-28 17:00:43 -0300

    module implementation

---
+++ js/login.js
@@ -0,0 +1,53 @@
+const $login_item = document.querySelector(".login");
+const $cadastro_item = document.querySelector(".cadastro");
+const $sesion_item = document.querySelector(".sesion");
+const $navbar = document.querySelector(".navbar");
+const $usuario = document.querySelector(".usuario");
+const $navbar_toogler = document.querySelector(".navbar-toggler");
+const $loader = document.querySelector(".loader");
+const $form_login = document.getElementById("form_login");
+$form_login.insertAdjacentHTML("afterend", `<p class="error_login"></p>`);
+const $error_login = document.querySelector(".error_login");
+
+export async function Revisar_Usuario(user, password) {
+  let devolver = false;
+  try {
+    let respuesta = await fetch("http://localhost:5555/user");
+    if (!respuesta.ok) {
+      throw { statusText: respuesta.statusText, status: respuesta.status };
+    }
+    let respuesta_json = await respuesta.json();
+    respuesta_json.forEach((element) => {
+      if (element.email === user && element.password === password) {
+        localStorage.user_medisol = JSON.stringify(element);
+        devolver = true;
+      }
+    });
+  } catch (error) {
+    console.log("error");
+    $loader.classList.remove("show");
+    let error_message = error.statusText || "Ocurrio un error";
+
+    $error_login.innerHTML = `${error_message}: ${error.status}`;
+    return null;
+  }
+  return devolver;
+}
+
+export function Actualizar_Sesion() {
+  try {
+    const usuario = JSON.parse(localStorage.user_medisol);
+    $login_item.classList.add("out");
+    $cadastro_item.classList.add("out");
+    $sesion_item.classList.add("active_drop");
+    $navbar.classList.add("active_drop");
+    $navbar_toogler.classList.add("active_drop");
+    $usuario.innerHTML = `${usuario.name}`;
+  } catch (error) {
+    $login_item.classList.remove("out");
+    $cadastro_item.classList.remove("out");
+    $sesion_item.classList.remove("active_drop");
+    $navbar.classList.remove("active_drop");
+    $navbar_toogler.classList.remove("active_drop");
+  }
+}
--- js/medisol.js
+++ js/medisol.js
@@ -1,56 +1,10 @@
-const $login = document.querySelector(".login");
+import { Actualizar_Sesion, Revisar_Usuario } from "./login.js";
 const $modal = document.querySelector(".modal");
 const $form_login = document.getElementById("form_login");
 $form_login.insertAdjacentHTML("afterend", `<p class="error_login"></p>`);
 const $error_login = document.querySelector(".error_login");
-const $login_item = document.querySelector(".login");
-const $cadastro_item = document.querySelector(".cadastro");
-const $sesion_item = document.querySelector(".sesion");
-const $navbar = document.querySelector(".navbar");
-const $usuario = document.querySelector(".usuario");
-const $navbar_toogler = document.querySelector(".navbar-toggler");
 const $loader = document.querySelector(".loader");
-/*Funciones*/
-const Revisar_Usuario = async (user, password) => {
-  let devolver = false;
-  try {
-    let respuesta = await fetch("http://localhost:5555/user");
-    if (!respuesta.ok) {
-      throw { statusText: respuesta.statusText, status: respuesta.status };
-    }
-    let respuesta_json = await respuesta.json();
-    respuesta_json.forEach((element) => {
-      if (element.email === user && element.password === password) {
-        localStorage.user_medisol = JSON.stringify(element);
-        devolver = true;
-      }
-    });
-  } catch (error) {
-    $loader.classList.remove("show");
-    let error_message = error.statusText || "Ocurrio un error";
-    $error_login.innerHTML = `${error_message}:${error.status}`;
-    return null;
-  }
-  return devolver;
-};
 
-const Actualizar_Sesion = () => {
-  try {
-    const usuario = JSON.parse(localStorage.user_medisol);
-    $login_item.classList.add("out");
-    $cadastro_item.classList.add("out");
-    $sesion_item.classList.add("active_drop");
-    $navbar.classList.add("active_drop");
-    $navbar_toogler.classList.add("active_drop");
-    $usuario.innerHTML = `${usuario.name}`;
-  } catch (error) {
-    $login_item.classList.remove("out");
-    $cadastro_item.classList.remove("out");
-    $sesion_item.classList.remove("active_drop");
-    $navbar.classList.remove("active_drop");
-    $navbar_toogler.classList.remove("active_drop");
-  }
-};
 /*Eventos del navegador*/
 document.addEventListener("click", (event) => {
   if (event.target.matches(".login")) {
@@ -69,20 +23,22 @@ document.addEventListener("click", (event) => {
     }
   }
 });
-document.addEventListener("submit", async (event) => {
+document.addEventListener("submit", (event) => {
   event.preventDefault();
   if (event.target.matches("#form_login")) {
-$loader.classList.add("show");
-    const user = event.target.user.value;
-    const password = event.target.password.value;
-    let coincide = await Revisar_Usuario(user, password);
-    if (coincide) {
-      //Aqui con la sesion abierta;
-      window.location.replace("../index.html");
-    } else if (coincide === false) {
-      $loader.classList.remove("show");
-      $error_login.innerHTML = "Usuario ou Senha Incorretos";
-    }
+    $loader.classList.add("show");
+    setTimeout(async () => {
+      const user = event.target.user.value;
+      const password = event.target.password.value;
+      let coincide = await Revisar_Usuario(user, password);
+      if (coincide) {
+        //Aqui con la sesion abierta;
+        window.location.replace("../index.html");
+      } else if (coincide === false) {
+        $loader.classList.remove("show");
+        $error_login.innerHTML = "Usuario ou Senha Incorretos";
+      }
+    }, 300);
   }
 });
 document.addEventListener("DOMContentLoaded", () => {
